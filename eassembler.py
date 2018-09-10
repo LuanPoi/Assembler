b1c1873e4f781044bae8bf72ceea1b55791637e4
Luan Patrick Alves Poi <luan.poi@pucpr.edu.br>
2018-09-10 12:51:31 -0300

    first commit

---
+++ eassembler.py
@@ -0,0 +1,12 @@
+def rawTreatment():
+    #colocar cada linha da lista em um vetor separando por espaços e vigulas
+    return toAsmInterpreter
+
+def asmInterpreter(asmCode, opCodes, regCodes):
+    #Remover todos os comentários
+    #While enquanto não encontrar o HALT
+        #comparar "asmCode[x]" com "opCodes"
+        #comparar "asmCode[x]" com "regCodes"
+    #colocar tudo em uma matriz separando cada comando em uma linha
+    #retornar essa matriz
+    return toCompile
