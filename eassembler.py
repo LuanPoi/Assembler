1f7fb493d05b6fa05aee21ef45f6b58a32980cc1
Andromeda <luan_poi@hotmail.com>
2018-09-21 10:55:08 -0300

    mexendo e estragando o codigo

--- eassembler.py
+++ eassembler.py
@@ -26,32 +26,21 @@ def carregarInstrucoes(arquivo: str):
 def processamento(arquivo: str, registradores: dict, instrucoes: dict):
     file = open(arquivo, 'r')
     linhas = file.readlines()
-    linhaSaida = []
     linhasSaida = []
-    print("testemerge")
+    print(linhas)
+    print("--##--##-- Debug --##--##--")
     for linha in linhas:
         # removendo a quebra de linha e os comentarios
-        token = linha.replace('\n', '').split(';')[0].strip().split(' ')
+        token = linha.splitlines()
+        token[0] = token[0].split(' ')
+        #token[0] = token[0].split(',')
+        #token[0] = token[0].split(';')
+        print(token)
         if len(token) == 1:
             if token[0].upper() in instrucoes.keys():
                 linhaSaida.append(instrucoes[token[0].upper()])
-
                 pass
 
-
-def inicio():
-    arqReg = r'/home/grad/taac2017s2/igor.barreto/Trabalhos/Montador-Simulador/registradores.txt'
-    arqIns = r'/home/grad/taac2017s2/igor.barreto/Trabalhos/Montador-Simulador/instrucoes.txt'
-    arqEntrada = r'/home/grad/taac2017s2/igor.barreto/Trabalhos/Montador-Simulador/entrada.txt'
-
-    reg = carregarRegistradores(arqReg)
-    inst = carregarInstrucoes(arqIns)
-    # print(carregarOpcode(arqOpc))
-    processamento(arqEntrada, reg, inst)
-
-
-if __name__ == "__main__":
-    inicio()
 
 def rawInputTreatment(entrada):
     #colocar cada linha da lista em um vetor separando por espaços e vigulas
@@ -69,3 +58,22 @@ def asmInterpreter(asmCode, opCodes, regCodes):
     #colocar tudo em uma matriz separando cada comando em uma linha
     #retornar essa matriz
     return toCompile
+
+def inicio():
+    arqReg = r'C:/Users/Andromeda/Assembler/REGCODES.esym'
+    arqIns = r'C:/Users/Andromeda/Assembler/REGCODES.esym'
+    arqEntrada = r'C:/Users/Andromeda/Assembler/codigofonte.txt'
+
+    '''arqReg = r'/home/grad/taac2017s2/igor.barreto/Trabalhos/Montador-Simulador/registradores.txt'
+    arqIns = r'/home/grad/taac2017s2/igor.barreto/Trabalhos/Montador-Simulador/instrucoes.txt'
+    arqEntrada = r'/home/grad/taac2017s2/igor.barreto/Trabalhos/Montador-Simulador/entrada.txt'''
+
+    reg = carregarRegistradores(arqReg)
+    inst = carregarInstrucoes(arqIns)
+    # print(carregarOpcode(arqOpc))
+
+    processamento(arqEntrada, reg, inst)
+
+
+if __name__ == "__main__":
+    inicio()
